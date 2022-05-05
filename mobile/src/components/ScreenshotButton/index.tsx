1f0e47a99bad92caecd05fd561aa1ae4f7677db9
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-05 18:03:59 -0300

    feature: created feedback form

---
+++ mobile/src/components/ScreenshotButton/index.tsx
@@ -0,0 +1,36 @@
+import { Camera, Trash } from 'phosphor-react-native';
+import { View, Image, TouchableOpacity } from 'react-native';
+import { theme } from '../../theme';
+
+import { styles } from './styles';
+
+interface ScreenshotButtonProps {
+  screenshot: string | null;
+  onTakeShot: () => void;
+  onRemoveShot: () => void;
+}
+
+export function ScreenshotButton({
+  screenshot,
+  onTakeShot,
+  onRemoveShot,
+}: ScreenshotButtonProps) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      {screenshot ? (
+        <View>
+          <Image source={{ uri: screenshot }} style={styles.image} />
+
+          <Trash
+            size={22}
+            color={theme.colors.text_secondary}
+            weight="fill"
+            style={styles.removeIcon}
+          />
+        </View>
+      ) : (
+        <Camera size={24} color={theme.colors.text_primary} weight="bold" />
+      )}
+    </TouchableOpacity>
+  );
+}
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -7,6 +7,11 @@ import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { theme } from '../../theme';
 import { styles } from './styles';
 import { Options } from '../Options';
+import { Form } from '../Form';
+
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -31,7 +36,8 @@ function Widget() {
         ref={bottomSheetRef}
         snapPoints={[1, 280]}
       >
-        <Options />
+        {/* <Options /> */}
+        <Form feedbackType="BUG" />
       </BottomSheet>
     </>
   );
